9df0a37949247fdb0d0d2fc11de8644f0eb6b822
Alfons Rovira <inclusa@gmail.com>
2016-07-04 13:58:26 +0200

    Disseny de funcions

---
+++ assets/py/milles_km.py
@@ -0,0 +1,12 @@
+
+    >>> def convertir_a_milles(quilometres):
+        '''(number) -> float
+        Torna un nombre equivalent de milles equivalent als quil`ometres.
+        >>>convertir_a_milles(34)
+        21.126620
+        >>>convertir_a_milles(234)
+        145.40086
+        '''
+        mi = quilometres * 0.62137
+        return mi
+
